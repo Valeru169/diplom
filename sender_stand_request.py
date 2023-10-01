68a70816a1bde2a6049ef2d68148a4f0d9fad62d
Valeru169 <144037266+Valeru169@users.noreply.github.com>
2023-10-02 01:10:30 +0700

    Update sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -4,7 +4,7 @@ import data
 
 def post_new_order(order_body): # функция получения ответа на запрос по созданию заказа
     return requests.post(configuration.URL_SERVICE + configuration.CREATING_AN_ORDER,
-                          json=data.order_body)
+                          json=order_body)
 
 def get_orders_trakt(track): # функция на получение ответа  заказа по треку
     return requests.get(configuration.URL_SERVICE + configuration.RECEIVING_AN_ORDER_BY_NUMBER,
